82840a2db367c07df1abc401cfec1df21fe4c373
Oleksandr Teteria <94607514+Alex-Teteria@users.noreply.github.com>
2025-10-05 13:47:09 +0300

    Update phone_bridge.py

--- phone_bridge.py
+++ phone_bridge.py
@@ -1,3 +1,9 @@
+# Author: Oleksandr Teteria
+# v0.2
+# 05.10.2025
+# Implemented and tested on Pi Pico with RP2040
+# Released under the MIT license
+
 import machine, time
 from machine import Pin, Timer, UART
 import json
@@ -288,3 +294,4 @@ uart.write(ber + '\n')
 # Run the main coroutine
 asyncio.run(main())
     
+
